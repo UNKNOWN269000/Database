cc2f8e894f3676edad2519dc1768743479582d9d
UNKNOWN269000 <102501982+UNKNOWN269000@users.noreply.github.com>
2026-07-11 18:04:01 +0530

    Delete src directory

--- src/components/PowdercoatPackingTable.tsx
+++
@@ -1,5 +0,0 @@
-import PackingTable from "./PackingTable";
-
-export default function PowdercoatPackingTable({ color }: { color: string }) {
-  return <PackingTable category="powdercoat_pack" color={color} title="Powdercoat Packing" />;
-}
